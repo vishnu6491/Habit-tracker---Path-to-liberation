4c7106c44af4515bdc435c2932268aca57aa2881
vishnu6491 <gemvishnu91@gmail.com>
2026-06-06 12:17:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/Habit-tracker---Path-to-liberation/',
   plugins: [
     react(),
     VitePWA({
