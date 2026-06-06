a3a6e420a7d40abbce154c8ec23abe0468b7eff7
vishnu6491 <gemvishnu91@gmail.com>
2026-06-06 10:40:27 +0530

    Add MainActivity for Capacitor integration

---
+++ android/app/src/main/android/app/src/main/java/com/habitquest/liberation/MainActivity.java
@@ -0,0 +1,7 @@
+package com.habitquest.liberation;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    // Capacitor automatically registers plugins defined in capacitor.config.json
+}
