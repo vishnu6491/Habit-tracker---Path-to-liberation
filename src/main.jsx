9aacfb00e02c147d921fa03305616d363d077be2
vishnu6491 <gemvishnu91@gmail.com>
2026-06-06 13:01:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-/*
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch(() => {});
-  });
-}
-*/
+console.log("MAIN LOADED");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 style={{color:'white'}}>APP STARTED</h1>
 );
