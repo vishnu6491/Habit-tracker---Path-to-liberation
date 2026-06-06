d99e01f3ed5409b029a2bf53840b14e88722b707
vishnu6491 <gemvishnu91@gmail.com>
2026-06-06 14:17:14 +0530

    Import constants from data module in helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,4 @@
+import { SAINT_LEVELS, MASTERY_QUESTS } from '../data/constants';
 export const getToday = () => new Date().toISOString().split('T')[0];
 
 export const calculateStreak = (logs) => {
