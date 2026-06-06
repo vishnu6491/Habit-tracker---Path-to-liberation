67ce769b3a66cdf1b292583a83023bfb2ea82c19
vishnu6491 <gemvishnu91@gmail.com>
2026-06-06 10:42:35 +0530

    Delete android/app/src/main/android/app/src/main/java/com/habitquest/liberation directory

--- android/app/src/main/android/app/src/main/java/com/habitquest/liberation/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.habitquest.liberation;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    // Capacitor automatically registers plugins defined in capacitor.config.json
-}
