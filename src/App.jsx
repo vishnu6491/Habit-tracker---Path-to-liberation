3bb0a5e97ff1f60394a21afb4e599014b02a1fb2
vishnu6491 <gemvishnu91@gmail.com>
2026-06-07 21:12:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // FIXED: HashRouter for PWA compatibility
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useApp } from './hooks/useApp';
 import BottomNav from './components/BottomNav';
 import Dashboard from './pages/Dashboard';
@@ -7,7 +7,6 @@ import Habits from './pages/Habits';
 import CalendarPage from './pages/Calendar';
 import Shop from './pages/Shop';
 import Reports from './pages/Reports';
-import Temple from './pages/Temple';
 import Settings from './pages/Settings';
 
 function App() {
@@ -23,7 +22,6 @@ function App() {
             <Route path="/calendar" element={<CalendarPage state={state} actions={actions} />} />
             <Route path="/shop" element={<Shop state={state} actions={actions} />} />
             <Route path="/reports" element={<Reports state={state} actions={actions} />} />
-            <Route path="/temple" element={<Temple state={state} actions={actions} />} />
             <Route path="/settings" element={<Settings state={state} actions={actions} />} />
           </Routes>
         </main>
