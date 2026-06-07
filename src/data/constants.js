358745c3f58c6567fd24604f0fca33e460395a48
vishnu6491 <gemvishnu91@gmail.com>
2026-06-07 20:37:24 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,55 +4,55 @@ export const SAINT_LEVELS = [
     name: 'Wanderer', 
     minProgress: 0, 
     env: 'Dusty Path', 
-    avatar: './avatars/wanderer.png', 
-    environment: './environments/dusty-path.png' 
+    avatar: '/avatars/wanderer.png', 
+    environment: '/environments/dusty-path.png' 
   },
   { 
     level: 2, 
     name: 'Disciplined Seeker', 
     minProgress: 15, 
     env: 'Forest', 
-    avatar: './avatars/seeker.png', 
-    environment: './environments/forest.png' 
+    avatar: '/avatars/seeker.png', 
+    environment: '/environments/forest.png' 
   },
   { 
     level: 3, 
     name: 'Yogi', 
     minProgress: 30, 
     env: 'Hermitage', 
-    avatar: './avatars/yogi.png', 
-    environment: './environments/hermitage.png' 
+    avatar: '/avatars/yogi.png', 
+    environment: '/environments/hermitage.png' 
   },
   { 
     level: 4, 
     name: 'Sage', 
     minProgress: 50, 
     env: 'Ashram', 
-    avatar: './avatars/sage.png', 
-    environment: './environments/ashram.png' 
+    avatar: '/avatars/sage.png', 
+    environment: '/environments/ashram.png' 
   },
   { 
     level: 5, 
     name: 'Master', 
     minProgress: 70, 
     env: 'Temple', 
-    avatar: './avatars/master.png', 
-    environment: './environments/temple.png' 
+    avatar: '/avatars/master.png', 
+    environment: '/environments/temple.png' 
   },
   { 
     level: 6, 
     name: 'Enlightened One', 
     minProgress: 85, 
     env: 'Celestial Realm', 
-    avatar: './avatars/enlightened.png', 
-    environment: './environments/celestial.png' 
+    avatar: '/avatars/enlightened.png', 
+    environment: '/environments/celestial.png' 
   },
   {     level: 7, 
     name: 'Liberation', 
     minProgress: 95, 
     env: 'Liberation Realm', 
-    avatar: './avatars/liberation.png', 
-    environment: './environments/liberation-realm.png' 
+    avatar: '/avatars/liberation.png', 
+    environment: '/environments/liberation-realm.png' 
   }
 ];
 
